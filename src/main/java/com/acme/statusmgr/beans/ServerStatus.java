7278dc8fd40e974365bf0824b64519149a5c4274
YosefSiebzener <jsiebzen2@student.touro.edu>
2022-12-06 02:48:29 -0500

    Changed ServerStatus to conform with superclass and the rest of the decorators.

--- src/main/java/com/acme/statusmgr/beans/ServerStatus.java
+++ src/main/java/com/acme/statusmgr/beans/ServerStatus.java
@@ -5,13 +5,7 @@ import com.acme.servermgr.ServerManager;
 /**
  * A POJO that represents Server Status and can be returned as the result of a request.
  */
-public class ServerStatus {
-
-
-    private long id;                // Unique identifier of request, sequential number
-    private String contentHeader;   // Some info about the request
-    private String statusDesc = "Unknown";  // the status being returned
-    private final Integer requestCost = 1;  // the cost in pennies of this request.
+public class ServerStatus extends DetailsBaseImplementation {
 
     /**
      * Construct a ServerStatus using info passed in for identification, and obtaining current
@@ -22,53 +16,9 @@ public class ServerStatus {
      * @param contentHeader info about the request
      */
     public ServerStatus(long id, String contentHeader) {
-        this.id = id;
-        this.contentHeader = contentHeader;
+        super(id, contentHeader, 1);
 
         // Obtain current status of server
-        this.statusDesc = "Server is " + ServerManager.getCurrentServerStatus();
+        setStatusDesc("Server is " + ServerManager.getCurrentServerStatus());
     }
-
-    public ServerStatus() {
-
-    }
-
-    /**
-     * get the id of this request
-     *
-     * @return a numeric id that increases during life of server for each request .
-     */
-    public long getId() {
-        return id;
-    }
-
-    /**
-     * Get the content header that was specified by the request
-     *
-     * @return some string
-     */
-    public String getContentHeader() {
-        return contentHeader;
-    }
-
-    /**
-     * Get an english-like description of the server's status
-     *
-     * @return A string describing status
-     */
-    public String getStatusDesc() {
-        return statusDesc;
-    }
-    public void setStatusDesc(String s) {
-        statusDesc = s;
-    }
-
-    /**
-     * Get the cost of this request
-     * @return Integer representing the cost of request as number of pennies
-     */
-    public Integer getRequestCost() {
-        return requestCost;
-    }
-
 }
