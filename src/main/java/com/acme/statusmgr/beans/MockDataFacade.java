17f1c568e35db2ada83c53e7bd1bf30d83c9f5c2
YosefSiebzener <jsiebzen2@student.touro.edu>
2022-12-08 14:28:52 -0500

    Added method javadocs.

--- src/main/java/com/acme/statusmgr/beans/MockDataFacade.java
+++ src/main/java/com/acme/statusmgr/beans/MockDataFacade.java
@@ -4,31 +4,56 @@ package com.acme.statusmgr.beans;
  * This class returns static values for testing purposes.
  */
 public class MockDataFacade implements SystemInformationFacadeInterface {
+
+    /**
+     * Returns fake value for available processors for testing purposes
+     * @return int
+     */
     @Override
     public int getAvailableProcessors() {
         return 4;
     }
 
+    /**
+     * Returns fake value for free memory for testing purposes
+     * @return long
+     */
     @Override
     public long getFreeMemory() {
         return 127268272;
     }
 
+    /**
+     * Returns fake value for total memory for testing purposes
+     * @return long
+     */
     @Override
     public long getTotalMemory() {
         return 159383552;
     }
 
+    /**
+     * Returns fake value for jre version for testing purposes
+     * @return String
+     */
     @Override
     public String getJreVersion() {
         return "15.0.2+7-27";
     }
 
+    /**
+     * Returns fake value for temp directory for testing purposes
+     * @return String
+     */
     @Override
     public String getTempLocation() {
         return "M:\\AppData\\Local\\Temp";
     }
 
+    /**
+     * Returns fake value for server status for testing purposes
+     * @return String
+     */
     @Override
     public String getServerStatus() {
         return "up";
